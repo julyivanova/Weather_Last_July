9bb9f2e417062e6184c5ba197a8dbe4908818c43
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 12:12:41 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li></li>Wednesday 7:00
+      </ul>
+      Hello from Weather
+    </div>
+  );
 }
