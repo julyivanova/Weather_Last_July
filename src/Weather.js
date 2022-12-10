7dc9e87e155d719496fbeeba02f40a4893c05dc0
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 12:27:07 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -6,12 +6,16 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <input
-            type="search"
-            placeholder="Enter a city.."
-            className="form-control"
-          />
-          <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
         </div>
       </form>
       <h1>New York</h1>
