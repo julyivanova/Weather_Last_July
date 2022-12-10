14e09254962b6c52796aa15579a440c4c2e9b87e
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 17:35:13 +0200

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{weatherData.city}</h1>
+      <h1>{props.data.city}</h1>
       <ul>
         <li>
-          <FormattedData date={weatherData.date} />
+          <FormattedData date={props.data.date} />
         </li>
-        <li className="text-capitalize">{weatherData.desciption}</li>
+        <li className="text-capitalize">{props.data.desciption}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
             <img
-              src={weatherData.iconUrl}
-              alt={weatherData.description}
+              src={props.data.iconUrl}
+              alt={props.data.description}
               className="float-left"
             />
             <div className="float-left">
               <span className="temperature">
-                {Math.round(weatherData.temperature)}
+                {Math.round(props.data.temperature)}
               </span>
               <span className="unit">°C</span>
             </div>
