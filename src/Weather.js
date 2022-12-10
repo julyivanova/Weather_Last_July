10e967175d5c882ca51192590f85caefa8bd5b1e
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 14:42:03 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -33,7 +34,7 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          6°C
+          <strong>6°C</strong>
         </div>
         <div className="col-6">
           <ul>
