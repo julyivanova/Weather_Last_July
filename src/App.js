7c1b7f84e84824549ff891096214349e3094aa47
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 10:57:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import react from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://shecodes.com/" target="_blank">
+          July
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
