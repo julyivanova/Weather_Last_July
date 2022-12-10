0a0678261a80255fc5fae6a8244dcc74e37127e0
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 10:50:31 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
+import react from "react";
 import "./App.css";
-import Weather from "./Weather";
-import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <Weather defaultCity="Kyiv" />
-          </div>
-        </div>
-        <footer className="text-center">
-          <a
-            href="https://github.com/julyivanova/React-Julia"
-            target="_blank"
-            rel="noreferrer"
-            className="text-decoration-none"
-          >
-            Open-source code
-          </a>
-          <br />
-          by July
-        </footer>
-      </div>
-    </div>
-  );
+export default function App() {
+  return <div className="App">Weather App</div>;
 }
-
-export default App;
