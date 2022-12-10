403be222072e9cc3cdea8fdb0566e5e6299bb1fa
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 11:08:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         This project was coded by{" "}
         <a href="https://shecodes.com/" target="_blank">
