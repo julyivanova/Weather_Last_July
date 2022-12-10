aa4a92d790b7dcc9e88daee09a10506f251f2bef
July <110743669+julyivanova@users.noreply.github.com>
2022-12-10 12:04:56 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://shecodes.com/" target="_blank">
-          July
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://shecodes.com/" target="_blank">
+            July
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
