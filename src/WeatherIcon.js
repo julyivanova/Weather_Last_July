cba4377184ade4c7d84281d8ff588ce503254c88
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 11:10:08 +0200

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -7,23 +7,12 @@ export default function WeatherIcon(props) {
     "01n": "CLEAR_NIGHT",
   };
 
-  if (props.code === "01d") {
-    return (
-      <ReactAnimatedWeather
-        icon="CLEAR_DAY"
-        color="#lelele"
-        size={64}
-        animate={true}
-      />
-    );
-  } else {
-    return (
-      <ReactAnimatedWeather
-        icon="CLEAR_NIGHT"
-        color="#lelele"
-        size={64}
-        animate={true}
-      />
-    );
-  }
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#lelele"
+      size={64}
+      animate={true}
+    />
+  );
 }
