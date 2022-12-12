d0cd8e94e087bee8bda150f8dade7add703d467f
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 14:27:03 +0200

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -28,7 +28,7 @@ export default function WeatherTemperature(props) {
     let fahrenheit = ((props.celsius * 9) / 5) * 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="temperature">{Math.round(fahrenheit)}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
