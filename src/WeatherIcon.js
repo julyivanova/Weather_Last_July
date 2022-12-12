fa20b4e4cbf7a1965e2560da864f7a97ffd9f49a
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 11:14:50 +0200

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -5,6 +5,22 @@ export default function WeatherIcon(props) {
   const codeMapping = {
     "01d": "CLEAR_DAY",
     "01n": "CLEAR_NIGHT",
+    "02d": "CLEAR_NIGHT",
+    "02n": "CLEAR_NIGHT",
+    "03d": "CLEAR_NIGHT",
+    "03n": "CLEAR_NIGHT",
+    "04d": "CLEAR_NIGHT",
+    "04n": "CLEAR_NIGHT",
+    "09d": "CLEAR_NIGHT",
+    "09n": "CLEAR_NIGHT",
+    "010d": "CLEAR_NIGHT",
+    "010n": "CLEAR_NIGHT",
+    "011d": "CLEAR_NIGHT",
+    "011n": "CLEAR_NIGHT",
+    "013d": "CLEAR_NIGHT",
+    "013n": "CLEAR_NIGHT",
+    "050d": "CLEAR_NIGHT",
+    "050n": "CLEAR_NIGHT",
   };
 
   return (
