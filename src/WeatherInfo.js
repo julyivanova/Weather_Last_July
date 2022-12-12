feb7495c6b70ddb1950696ff92089466f9fbdff0
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 10:57:38 +0200

    upd

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,11 +20,6 @@ export default function WeatherInfo(props) {
                 code={props.data.icon}
                 alt={props.data.description}
               />
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="float-left"
-              />
             </div>
             <div className="float-left">
               <span className="temperature">
