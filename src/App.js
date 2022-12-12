15ed3ead345fd10e13f083638829b3de8a8b988a
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 14:30:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ export default function App() {
         <Weather defaultCity="Kyiv" />
         <footer>
           This project was coded by{" "}
-          <a href="https://shecodes.com/" target="_blank">
+          <a
+            href="https://www.shecodes.io/graduates/52089-july-ivanova"
+            target="_blank"
+          >
             July
           </a>{" "}
           and is{" "}
-          <a href="https://github.com/" target="_blank">
+          <a
+            href="https://github.com/julyivanova/Weather_Last_July"
+            target="_blank"
+          >
             open-sourced on Github
           </a>
         </footer>
