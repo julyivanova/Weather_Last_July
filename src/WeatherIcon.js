676c508d9a6c3891038d7dfdf0a31350593a404e
July <110743669+julyivanova@users.noreply.github.com>
2022-12-12 11:03:03 +0200

    upd

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -5,7 +5,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon="CLEAR_DAY"
-      color="red"
+      color="#1d6ef8"
       size={64}
       animate={true}
     />
